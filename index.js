f2341800dc5abd8c014a68ce2a980ff08289dffd
GeronimoNeto <102226234+GeronimoNeto@users.noreply.github.com>
2022-09-15 14:25:26 -0300

    soma

--- index.js
+++ index.js
@@ -5,9 +5,23 @@ app.use(cors())
 const http = require("http")
 const server = http.createServer(app)
 
-app.get('/calc/:value',(req,res)=>{
-    const value = req.val
-    console.log(value)
+app.get('/calc/:val',(req,res)=>{
+    const value = req.params.val
+
+    const splitPlus = value.split("+")
+    let numPlus = 0
+    splitPlus.forEach((num)=>{
+        numPlus+= Number(num)
+    })
+
+    const splitMinus = value.split("-")
+    let numMinus = 0
+    splitMinus.forEach(()=>{
+        
+    })
+
+    const final = numPlus
+    console.log(final)
 })
 
 server.listen(999,()=>{
